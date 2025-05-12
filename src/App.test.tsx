40322e869c4ec9d38dcf1b525de642d99b031ab7
akansha010293 <akansha@see-mode.com>
2025-05-12 22:24:46 +1000

    test: add page load test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { TreeProvider } from "./hooks/useTreeContext";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders page header", () => {
+  render(
+    <TreeProvider>
+      <App />
+    </TreeProvider>
+  );
+  const header = screen.getByText(/Folder Structure Maker/i);
+  expect(header).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  render(
+    <TreeProvider>
+      <App />
+    </TreeProvider>
+  );
+  const addButton = screen.getByRole("button", {
+    name: /Add root folder/i,
+  });
+  expect(addButton).toBeInTheDocument();
 });
